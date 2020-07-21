112096f75471b7e640d3b0c7c681fa2a0e0955ee
Ajay Yadav <ajayyadav.asm@gmail.com>
2020-07-22 01:28:26 +0530

    Implemented devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'portfolio-details/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'home', to: 'pages#home'
